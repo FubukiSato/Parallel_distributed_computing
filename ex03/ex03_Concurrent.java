dae80aae540674f3cec4af4adf8a8d728d398426
FubukiSato <satofubuki0510@gmail.com>
2021-12-16 23:02:04 +0900

    edit

--- ex03/ex03_Concurrent.java
+++ ex03/ex03_Concurrent.java
@@ -7,20 +7,60 @@ public class ex03_Concurrent {
         Integer process_num = scanner.nextInt();
         scanner.close();
         Thread[] th = new Thread[process_num];
+        Game[] game = new Game[process_num];
         for(int i=0;i<process_num;i++){
-            th[i] = new game(i+1);
+            game[i] = new Game(i+1);
+        }
+        
+        for(int i=0;i<process_num;i++){
+            th[i] = game[i];
+        }
+        System.out.println(game[0].get_identifier());
+        int i=1;
+        int max=-1;
+        int flag=0;
+        while(flag == 0){
+            for(int j=0;j<process_num;j++){
+                if(game[j].getnum_sum() == 3){
+                    System.out.println("勝者: "+ game[j].get_identifier());
+                    flag++;
+                }
+                break;
+            }
+
+            for(int k=0;k<process_num;k++){
+                th[k].start();
+                System.out.println(game[k].getmax_num());
+            }
+            System.out.println("");
+            
+
+            System.out.println("ラウンド"+i+"の勝者"); i++;
+            for(int j=0;j<process_num;j++){
+                System.out.println(game[i].getmax_num());
+                if(max < game[i].getmax_num()){
+                    System.out.println(max);
+                    max = game[i].getmax_num();
+                }
+            }
+            for(int j=0;j<process_num;j++){
+                if(game[i].getmax_num() == max){
+                    System.out.print(game[i].get_identifier()+" ");
+                }
+            }
         }
     }
 }
 
-    class game extends Thread{
+    class Game extends Thread{
         Random random = new Random();
 
-        game(int i){
+        Game(int i){
             this.identifier = i;
         }
         public void run(){
             this.max_num = random.nextInt(10);
+            System.out.println(this.max_num);
         }
         public int getmax_num(){
             return this.max_num;
